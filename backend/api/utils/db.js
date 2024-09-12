b489efd22646253942858f2d0b819b7a08f66f4f
olehmarushchak <westernpc22@gmail.com>
2024-09-12 12:13:25 +0200

    add dialect fix

--- backend/api/utils/db.js
+++ backend/api/utils/db.js
@@ -1,18 +1,18 @@
 "use strict";
+const pg = require("pg");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const { Sequelize } = require("sequelize");
 
-const {
-  POSTGRES_URL
-} = process.env;
+const { POSTGRES_URL } = process.env;
 
 console.log(POSTGRES_URL);
 
 const sequelize = new Sequelize(POSTGRES_URL, {
   dialect: "postgres",
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true, // Ensures SSL connection for security
